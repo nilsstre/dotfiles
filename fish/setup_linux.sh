8f1fafafee2d8a5a4986b1906cf4452b6e4e048a
nilsstre <nils.streijffert@sellpy.se>
2020-08-30 17:55:43 +0200

    More fixes

--- fish/setup_linux.sh
+++ fish/setup_linux.sh
@@ -6,10 +6,10 @@ apt upgrade
 
 yes | apt install fish
 
-chsh -s (which fish)
+chsh -s /usr/bin/fish
 curl -L http://get.oh-my.fish | fish
 
-fish ~/github/private/dotfiles/fish/bobthefish_linux.sh
+fish ./fish/bobthefish_linux.sh
 
 ln -isv ./fish/config.fish ~/.config/fish/config.fish
 ln -isv ./fish/functions ~/.config/fish/functions
