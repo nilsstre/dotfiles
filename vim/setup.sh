d20540d1390db663727601d028099ef3826e145a
nilsstre <nils.streijffert@sellpy.se>
2020-08-30 17:23:58 +0200

    Update

--- vim/setup.sh
+++ vim/setup.sh
@@ -6,8 +6,8 @@ mkdir -p ~/.vim ~/.nvim ~/.config/nvim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -isv ~/github/private/dotfiles/vim/vimrc ~/.vim/vimrc
-ln -isv ~/github/private/dotfiles/vim/init.vim ~/.config/nvim/init.vim
+ln -isv ./vim/vimrc ~/.vim/vimrc
+ln -isv ./vim/init.vim ~/.config/nvim/init.vim
 
 # install plugins
 nvim -c ':PlugInstall' -c 'qa!'
