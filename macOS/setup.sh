1b6cb86c5213e65fc2954f397062d53d9b2cc584
Nils Streijffert <nils.streijffert@gmail.com>
2020-11-04 14:23:58 +0100

    Fix paths

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -2,6 +2,8 @@
 
 # Based on https://gist.github.com/bradp/bea76b16d3325f5c47d4
 
+echo "Started MacOS setup script"
+
 email="nils.streijffert@gmail.com"
 
 # Ask for the administrator password upfront
@@ -24,48 +26,43 @@ brew update
 echo "Installing Homebrew cask"
 brew install caskroom/cask/brew-cask
 
-echo "Installing Chrome..."
-brew cask install google-chrome
-
 echo "Installing Git..."
 brew install git
 
 echo "Creating an SSH key for you..."
 mkdir -p ~/.ssh
-cd ~/.ssh
-touch github
-ssh-keygen -t rsa -b 4096 -C $email
-chmod 400 github
-ssh-add github
+github=~/.ssh/github
+touch $github
+ssh-keygen -t rsa -b 4096 -C $email -f $github
+chmod 400 $github
+ssh-add $github
+cat $github.pub
 
 echo "Please add this public key to Github \n"
 echo "https://github.com/account/ssh \n"
 read -p "Press [Enter] key after this..."
 
-echo "Setting up Git"
-bash ~/github/private/dotfiles/macOS/scripts/git.sh
-
-echo "Copying dotfiles from Github"
+echo "Pulling dotfiles from Github"
 mkdir -p ~/github/private
 cd ~/github/private
 git clone git@github.com:nilsstre/dotfiles.git dotfiles
 
 # Run mac setup script
-bash ~/github/private/dotfiles/macOS/scripts/mac.sh
+bash ~/github/private/dotfiles/macOS/scripts/mac.sh || echo "Error while running the mac.sh script"
 
 # Install applications and packages using Homebrew
-bash ~/github/private/dotfiles/macOS/scripts/packages.sh
+bash ~/github/private/dotfiles/macOS/scripts/packages.sh || echo "Error while running the packages.sh script"
 
 # Setup Vim
-bash ~/github/private/dotfiles/vim/vim.sh
+bash ~/github/private/dotfiles/vim/setup.sh || echo "Error while running the vim setup script"
 
 # Setup Git
-bash ~/github/private/dotfiles/git/setup.sh
+bash ~/github/private/dotfiles/git/setup.sh || echo "Error while running the git setup script"
 
 # Run fish script
-bash ~/github/private/dotfiles/fish/setup_mac.sh
+bash ~/github/private/dotfiles/fish/setup_mac.sh || echo "Error while running the fish_mac setup script"
 
 # Setup nvm
-fish ~/github/private/dotfiles/nvm/setup.sh
+fish ~/github/private/dotfiles/nvm/setup.sh || echo "Error while running the vim setup script"
 
 echo "Done!"
