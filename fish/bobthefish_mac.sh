2713e505b3a1c24bd5e03fbde0ddac57ccc54950
Nils Streijffert <nils.streijffert@gmail.com>
2021-08-30 16:17:19 +0200

    Fixed merge issue

--- fish/bobthefish_mac.sh
+++ fish/bobthefish_mac.sh
@@ -7,11 +7,7 @@ brew install --cask font-fira-code-nerd-font
 set -U theme_nerd_fonts yes
 set -U theme_project_dir_length 1
 
-<<<<<<< HEAD
-sh (brew --prefix)/opt/fzf/install
-=======
 omf install bass
 omf install nvm
 
 sh (brew --prefix)/opt/fzf/install
->>>>>>> a78b17cb0cef42a5277fc8b5e6892265b0d7df4a
