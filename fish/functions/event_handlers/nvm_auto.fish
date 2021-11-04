eaced3269a22c30b60e9fb9aa3cf233d995101df
Nils Streijffert <nils.streijffert@gmail.com>
2021-11-04 16:55:50 +0100

    Updated description

--- fish/functions/event_handlers/nvm_auto.fish
+++ fish/functions/event_handlers/nvm_auto.fish
@@ -1,8 +1,8 @@
-function nvm_auto --on-variable PWD --description 'Do rvm stuff'
+function nvm_auto --on-variable PWD --description 'Auto switch node version based on .nvmrc file'
   status --is-command-substitution; and return
 
   if test -e .nvmrc
     nvm use (cat .nvmrc)
     echo Using node version: (node -v)
-    end
+  end
 end
