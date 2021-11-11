7be6ea5282c7ef702547c253aa9251e1c9ee523f
Nils Streijffert <nils.streijffert@gmail.com>
2021-11-11 13:29:49 +0100

    Removed root setup script

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$(uname)" == "Darwin" ]; then
-    curl https://raw.githubusercontent.com/nilsstre/dotfiles/master/macOS/setup.sh | bash
-elif [ "$(uname)" == "Linux" ]; then
-    curl https://raw.githubusercontent.com/nilsstre/dotfiles/master/linux/setup.sh | bash
-fi
