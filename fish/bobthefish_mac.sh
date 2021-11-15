0afd3373da60920e3a9b1538ce23205199871031
Nils Streijffert <nils.streijffert@gmail.com>
2021-11-15 13:39:16 +0100

    Removed brew stuff

--- fish/bobthefish_mac.sh
+++ fish/bobthefish_mac.sh
@@ -1,13 +1,10 @@
 #!/usr/local/bin/fish
 
 omf install bobthefish
-brew tap homebrew/cask-fonts
-brew install --cask font-fira-code-nerd-font
+omf install bass
+omf install nvm
 
 set -U theme_nerd_fonts yes
 set -U theme_project_dir_length 1
-
-omf install bass
-omf install nvm
 
 sh (brew --prefix)/opt/fzf/install
