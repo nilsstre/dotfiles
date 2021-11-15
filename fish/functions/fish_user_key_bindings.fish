9f61721435e0f5ffad7d8a06b719a02bd97b84f3
Nils Streijffert <nils.streijffert@gmail.com>
2021-11-15 13:25:15 +0100

    Moved the FZF_DEFAULT_COMMAND variable

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,7 @@
 function fish_user_key_bindings --description "ctrl-p searches files with fzf and opens in vim"
-  # See FZF_DEFAULT_COMMAND env variable for ignore files/folders
+	# Use ripgrep for fzf by default
+	set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow -g "!{.git,node_modules,.DS_Store,Library,Applications,.npm,.zsh_sessions,.fzf,.cache}/*" 2> /dev/null'
+
 	bind \cp 'fzf | read -l result; and vim $result';
 	fzf_key_bindings
 end
