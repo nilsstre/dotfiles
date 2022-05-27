2d4ebee82bbbbb4cabaa53b942d6f1cd9dd4bade
Nils Streijffert <nils.streijffert@gmail.com>
2022-05-27 22:33:18 +0200

    Fix

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
+# Use ripgrep for fzf by default
+set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow -g "!{.git,node_modules,.DS_Store,Library,Applications,.npm,.zsh_sessions,.fzf,.cache,}/*" 2> /dev/null'
+
 set -x GREP_OPTIONS '--color=auto'
 set -x GREP_COLOR '1;30;40'
+
 
 # Event handler functions needs to be source directly in the config file to work
 for func in (find ~/.config/fish/functions -name 'event_*.fish')
@@ -14,6 +18,7 @@ if test -d $tools_bin
 end
 
 # Operating system specific configuration
+
 switch (uname)
     case Darwin
         set iterm2_integration_file ~/.iterm2_shell_integration.fish
@@ -40,3 +45,5 @@ end
 if test -q $navi
     navi widget fish | source
 end
+
+fish_add_path /usr/local/opt/openjdk/bin
