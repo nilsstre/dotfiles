775fb2ebf9ace0701c2eee1bb644511cba1501a4
Nils Streijffert <nils.streijffert@gmail.com>
2024-12-13 15:45:18 +0100

    Removed old stuff

--- git/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# symlink stuff
-ln -isv $script_dir/gitconfig ~/.gitconfig
-ln -isv $script_dir/gitignore ~/.gitignore
--- macOS/packages/ovpn.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Downloading OVPN-client"
-OVPN=~/Downloads/OVPN.zip
-curl -L https://files.ovpn.com/client/latest/OVPN.zip -o $OVPN
-unzip $OVPN -d ~/Downloads/
-rm -rf $OVPN
