046685ebf31059e3f1c60d97339740a5202fbf3a
Nils Streijffert <nils.streijffert@gmail.com>
2025-07-06 14:52:01 +0200

    Updated indent-blnacline config

--- nvim/lua/config/lazy/indent-blankline.lua
+++ nvim/lua/config/lazy/indent-blankline.lua
@@ -1,7 +1,29 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPost", "BufNewFile" },
 	main = "ibl",
 	---@module "ibl"
-	---@type ibl.config
-	opts = {},
+	---@type table
+	opts = {
+		indent = {
+			char = "│",
+			tab_char = "│",
+		},
+		scope = { enabled = false },
+		exclude = {
+			filetypes = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+			},
+		},
+	},
 }
